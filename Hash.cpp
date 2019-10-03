216707e158de8a5d5b03623d11fb076fa232ac98
Augustinas Makevičius <amakevicius98@gmail.com>
2019-10-03 11:32:36 +0300

    ADD: Hash cpp/h.

---
+++ Hash.cpp
@@ -0,0 +1,16 @@
+//
+// Created by amake on 19/10/03.
+//
+
+#include "Headers/Hash.h"
+
+string racerHash(string input) {
+
+    string hash;
+
+    // YOUR CODE GOES HERE! IT IS EXPEXTED THAT YOU RETURN A STRING.
+    // If you need to create other functions for recusion or such please declare them in this file.
+
+    return hash;
+
+}
